561c03a9cf72b42900c31080dcc7df4621359459
Joshua-He <joshua5089@gmail.com>
2016-12-02 20:35:04 -0800

    Add files via upload

---
+++ Music.cpp
@@ -0,0 +1,34 @@
+Last login: Fri Dec  2 19:44:37 on console
+Joshuas-MBP:~ joshuahe$ ls
+#Media.cpp#	Desktop		Library		Music
+Applications	Documents	Media.cpp	Pictures
+C++		Downloads	Media.h		Public
+Classes.cpp	Google Drive	Movies		test.cpp~
+Joshuas-MBP:~ joshuahe$ cd C++
+Joshuas-MBP:C++ joshuahe$ ls
+Joshuas-MBP:C++ joshuahe$ ls
+Classes.cpp	Media.h		Movies.h	VideoGame.h
+Media.cpp	Movies.cpp	VideoGame.cpp
+Joshuas-MBP:C++ joshuahe$ emacs Music.cpp
+
+
+
+
+
+  cout << "enter artist" << endl;
+  cin >> artist;
+  cout << "enter year" << endl;
+  cin >> year;
+  cout << "enter publisher" << endl;
+  cin >> publisher;
+  cout << "enter duration" << endl;
+  cin >> duration;
+}
+void Music::displayInfo(){
+  cout << "Movie info: " << title << ", " << artist << ", " << year << "\
+, " << publisher << ", " << duration <<  endl;
+
+}
+
+-uuu:**-F1  Music.cpp      Bot L27    (C++/l Abbrev)---------------------
+
---
+++ Music.h
@@ -0,0 +1,48 @@
+Last login: Fri Dec  2 19:51:29 on ttys007
+Joshuas-MBP:~ joshuahe$ cd C++
+Joshuas-MBP:C++ joshuahe$ emacs Music.h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+class Music{
+ public:
+  Music();
+  char artist[100];
+  char publisher[100];
+  char duration[100];
+  void setInfo();
+  void displayInfo();
+
+
+
+};
+
+
+
+
+
+
+
+
+-uuu:**-F1  Music.h        All L14    (C/l Abbrev)------------------------------
+
